0b29a00a8c0037c5c60f3c64e3170199c5ee56fa
SSimona03 <79479935+SSimona03@users.noreply.github.com>
2021-05-18 23:15:09 +0100

    Added pollen data to ui (#1)
    * Added pollen data to ui
    
    * remove space

--- weather-frontend/weather.js
+++ weather-frontend/weather.js
@@ -36,6 +36,68 @@ const getWeather = async (location) => {
     }
 };
 
+const getPollen = async (location) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/pollen?location=${location}`);
+  
+    const pollenItems = response.data;
+
+    console.log(`GET: Here's the list of pollen`, pollenItems);
+
+    return pollenItems;
+  } catch (err) {
+    console.error(err);
+    throw err
+  }
+};
+
+const changeWeatherDetails = (weatherData) => {
+  const degreeElement = document.getElementById('degreeText')
+  degreeElement.innerHTML = Math.round(weatherData.main.temp - 273.15) + "°";
+
+  const degreeMinElement = document.getElementById('minTemp');
+  degreeMinElement.innerHTML = Math.round(weatherData.main.temp_min - 273.15) + "°";
+
+
+  const degreeMaxElement = document.getElementById('maxTemp');
+  degreeMaxElement.innerHTML = Math.round(weatherData.main.temp_max - 273.15) + "°";
+
+
+  const cityElement = document.getElementById('city');
+  cityElement.innerHTML = weatherData.name;
+
+
+  const descriptionElement = document.getElementById('description');
+  descriptionElement.innerHTML = weatherData.weather[0].description;
+
+
+  const pressureElement = document.getElementById('press');
+  pressureElement.innerHTML = weatherData.main.pressure;
+}
+
+const removePollenStatusClasses = (element) => {
+  element.classList.remove("moderate");
+  element.classList.remove("high");
+  element.classList.remove("low");
+}
+
+const changePollenData = (pollenData) => {
+  const treeElement = document.getElementById('treePollenStatus');
+  treeElement.innerHTML = pollenData.Risk.tree_pollen;
+  removePollenStatusClasses(treeElement);
+  treeElement.classList.add(pollenData.Risk.tree_pollen.toLowerCase());
+
+  const grassElement = document.getElementById('grassPollenStatus');
+  grassElement.innerHTML = pollenData.Risk.grass_pollen;
+  removePollenStatusClasses(grassElement);
+  grassElement.classList.add(pollenData.Risk.grass_pollen.toLowerCase());
+
+  const weedElement = document.getElementById('weedPollenStatus');
+  weedElement.innerHTML = pollenData.Risk.weed_pollen;
+  removePollenStatusClasses(weedElement);
+  weedElement.classList.add(pollenData.Risk.weed_pollen.toLowerCase());
+}
+
 window.onload = async (event) => {            
     getClock();
     
@@ -47,31 +109,12 @@ window.onload = async (event) => {
 
       try{
         const cityName = event.target.value;
-        const weatherData = await getWeather(cityName);
-  
-        const degreeElement = document.getElementById('degreeText')
-        degreeElement.innerHTML = Math.round(weatherData.main.temp - 273.15) + "°";
-
-
-
-        const degreeMinElement = document.getElementById('minTemp');
-        degreeMinElement.innerHTML = Math.round(weatherData.main.temp_min - 273.15)+ "°";
-
-
-        const degreeMaxElement = document.getElementById('maxTemp');
-        degreeMaxElement.innerHTML = Math.round(weatherData.main.temp_max - 273.15)+ "°";
-
-
-        const cityElement = document.getElementById('city');
-        cityElement.innerHTML = weatherData.name;
-
-
-        const descriptionElement = document.getElementById('description');
-        descriptionElement.innerHTML = weatherData.weather[0].description;
         
-
-        const pressElement = document.getElementById('press');
-        pressElement.innerHTML = weatherData.main.pressure;
+        const weatherData = await getWeather(cityName);
+        changeWeatherDetails(weatherData);
+        
+        const pollenData = await getPollen(cityName);
+        changePollenData(pollenData);
 
         console.log(` ${event.target.value}`);
 
